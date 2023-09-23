6b17f58eb84618f2f84daa5bb7514afe5071a135
我真的是黄海华 <>
2023-09-23 13:50:20 +0800

    RxJava 3 线程切换

--- RxJava/app/src/main/java/com/demo/rxjava/source/RxjavaActivity1.java
+++ RxJava/app/src/main/java/com/demo/rxjava/source/RxjavaActivity1.java
@@ -12,8 +12,10 @@ import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 
 public class RxjavaActivity1 extends AppCompatActivity {
 
@@ -22,6 +24,19 @@ public class RxjavaActivity1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rxjava1);
 
+        /*
+                    ObservableCreate(source0) extends Observabl {subscribeActual}   => observer4
+                ObservableMap(source1,   => observer3
+            ObservableSubscribeOn(source2,（即source1）  => observer2
+                source2.subscribeActual执行在 subscribeOn分配的线程中
+        ObservableObserveOn(source3,   => observer1
+        .subscribe(Observer 自定义) => observer0
+
+        1、执行 .subscribe时，source3.subscribe -> source2 -> source1 -> source0,
+        2、.subscribe操作看子类实现的subscribeActual接口，目的是把接入入参的自定义Observer一层层包裹起来
+           observer0 -> observer1 -> observer2 -> observer3 -> observer4
+        3、observer4.onNext -> observer3.onNext ->  observer2.onNext -> observer1.onNext -> observer0.onNext
+         */
         Observable.create(new ObservableOnSubscribe<Object>() {
             @Override
             public void subscribe(@NonNull ObservableEmitter<Object> e) throws Exception {
@@ -36,7 +51,14 @@ public class RxjavaActivity1 extends AppCompatActivity {
                         return null;
                     }
                 })
+                // 给上面 执行线程
+                /* scheduler.scheduleDirect(new SubscribeTask(parent))
+                线程池 执行 Runnable
+                 */
+                .subscribeOn(Schedulers.io())
                 // ObservableMap.subscribe
+                // 给下面 执行主线程
+                .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Observer<Object>() {
                     @Override
                     public void onSubscribe(@NonNull Disposable d) {
@@ -97,6 +119,6 @@ public class RxjavaActivity1 extends AppCompatActivity {
                     public void onComplete() {
 
                     }
-                })
+                });
     }
 }
